a061db9c8867d9b473f41f1dafa0bc159f84103c
2silverballs <sqwelshy@gmail.com>
2020-04-17 15:42:49 +0100

    Added Sun

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -13,6 +13,10 @@ app.get('/moon', function(req, res) {
   res.send('Hello Moon!\n');
 });
 
+app.get('/sun', function(req, res) {
+  res.send('Hello Sun!\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
